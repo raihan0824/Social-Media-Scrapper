952146eb032197e23912995dd4403d311a0552c7
raihan <raihanafiandi@gmail.com>
2024-05-08 08:41:43 +0700

    change headers

--- server/routes/main_services.py
+++ server/routes/main_services.py
@@ -257,8 +257,14 @@ async def scrape_ta(url: str):
 @scraping_router.get("/api/v1/convert-facebook-url")
 def convert_fb_url(url: str):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         link_header = response.headers.get('Link')
