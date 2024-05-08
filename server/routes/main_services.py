13781595e545a6abef49388d8bc45b020211ff94
raihan <raihanafiandi@gmail.com>
2024-05-08 13:08:35 +0700

    testing

--- server/routes/main_services.py
+++ server/routes/main_services.py
@@ -302,7 +302,7 @@ async def scrape_facebook(url: str):
             page = await context.new_page()
             page.on("response", intercept_response)
             await page.goto(url)
-            await page.wait_for_load_state('load')
+            time.sleep(5)
             # await page.wait_for_selector("[data-testid='tweetText']",timeout=3000)
             tweet_calls = [f for f in _xhr_calls if "https://www.facebook.com/ajax/bulk-route-definitions/" in f.url][0]
             data_raw = await tweet_calls.text()
