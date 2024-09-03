b56db8c17a2883344b3dc067421669315b537a25
louis-not <mayco.louis@gmail.com>
2024-09-03 16:55:04 +0700

    change output

--- server/routes/image_services.py
+++ server/routes/image_services.py
@@ -15,27 +15,45 @@ logger = logging.getLogger('Image-Scraper')
 
 
 @image_router.get("/api/v1/scrape-image-google")
-def scrape_image_google(queries: Optional[List[str]]=Query(None)):
+def scrape_image_google(queries: Optional[List[str]] = Query(None)):
     query_counter = Counter(queries)
-    results = []
+    
+    result_dict = {}
     for query, num_images in query_counter.items():
         urls = asyncio.run(google_get_image(query, n=num_images))
-        for url in urls:
-            results.append((query, url))
+        result_dict[query] = urls
+
+    results = []
+    for query in queries:
+        urls = result_dict[query]
+        index = queries.index(query) % len(urls)
+        results.append({
+            'query': query,
+            'url': urls[index]
+        })
+
     return results
 
 @image_router.get("/api/v1/scrape-image-x")
 def scrape_image_x(queries: Optional[List[str]]=Query(None)):
     query_counter = Counter(queries)
-    results = []
+    result_dict = {}
     for query, num_images in query_counter.items():
         url = convert_query_to_url(query)
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         urls = loop.run_until_complete(scrape_search_result(url, []))
         urls_chosen = random.choices(urls, k=num_images)
-        for url in urls_chosen:
-            results.append((query, url))
+        result_dict[query] = urls_chosen
 
+
+    results = []
+    for query in queries:
+        urls = result_dict[query]
+        index = queries.index(query) % len(urls)
+        results.append({
+            'query': query,
+            'url': urls[index]
+        })
 
     return results
